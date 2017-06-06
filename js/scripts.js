d3004ba0aa186d55f10e1bd91045ec211a323fc1
jacob ruleaux and Michelle Poterek <jacob@>
2017-06-06 09:24:21 -0700

    establish html template and js template

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#form1").submit(function(event) {
+    var numbers = ["number1", "number2"];
+
+      for (var number = 0; number <= "#number2"; number += "#number1") {
+      alert(("<li>") + count + ("</li>"));
+    }
+
+  event.preventDefault();
+  });
+});
