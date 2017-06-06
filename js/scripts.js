9b03212f4d4c2f1fde79c7102341e504b456c592
jacob ruleaux and Michelle Poterek <michelle@>
2017-06-06 10:55:42 -0700

    fix functionality of for loop by putting the for statement within the branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 $(document).ready(function() {
   $("#form1").submit(function(event) {
+    event.preventDefault();
     var countTo = parseInt($("input#number2").val());
     var countBy =  parseInt($("input#number1").val());
-    var total = 0
-      for (var number = 0; number <= countTo; number += countBy) {
-        total += number;
+    console.log(countTo);
+    console.log(countBy);
+    // var numbers= [];
+      // console.log(total);
+  if (countTo < countBy) {
+    alert("WRoNG!");
+  } else if (countTo < 0 || countBy < 0) {
+      alert("IDiOT!");
+  } else if (isNaN(countBy) || isNaN(countTo)) {
+    alert("Bonehead!");
+  } else {
+      for (var index = countBy; index <= countTo; index += countBy) {
+        alert(index);
+      }
+  }
 
-    }
-    alert(("<li>") + total + ("</li>"));
-  event.preventDefault();
+
+
   });
 });
